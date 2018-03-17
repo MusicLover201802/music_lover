90b567a26affeb9dd10b4c1b842a53dd5f8a1cf8
Kaito-Fukai <yaiag04@gmail.com>
2018-03-17 07:04:56 +0000

    購入機能作成中 購入した分在庫から引くアクション完成 これから履歴保存＋削除機能実装へ

--- app/controllers/lovers/orders_controller.rb
+++ app/controllers/lovers/orders_controller.rb
@@ -1,12 +1,28 @@
 class Lovers::OrdersController < ApplicationController
-  def new
-  	@user = User.find(current_user.id)
-  	@user_items = @user.user_items
-  	@neworder = Order.new
-  	@neworder.save
-  end
+	def new
+		@user = User.find(current_user.id)
+		@user_items = @user.user_items
+		@neworder = Order.new
+		@neworder.save
+	end
 
-  def show
-  	@order = Order.find(params[:id])
-  end
+	def show
+		@order = Order.find(params[:id])
+	end
+
+	def create
+		@neworder = Order.new(order_params)
+		@neworder.save
+		@neworderitem = OrderItem.new(order_item_params)
+		@neworderitem.save
+		redirect_to controller: 'user_items', action: 'destroy', id: current_user.id
+	end
+
+  private
+	def order_params
+		params.require(:order).permit(:)
+	end
+
 end
+
+
--- app/controllers/lovers/user_items_controller.rb
+++ app/controllers/lovers/user_items_controller.rb
@@ -4,10 +4,35 @@ class Lovers::UserItemsController < ApplicationController
   	@user_items = @user.user_items
   end
 
-  def create
+  def method
+    # カートの中身全取得
+    @user = User.find(current_user.id)
+    @user_items = @user.user_items
+
+    # 在庫減らす処理
+    @user_items.each do |user_items|
+    user_items.item.stock -= user_items.quantity
+    user_items.item.save
+    end
+
+    # 購入履歴保存アクションへリダイレクト
+    # redirect_to
+
   end
 
+
+
+
+
+    # カートの中身を全取得する find_by(カートid)
+    #   each文内で
+    #     在庫を減らす count -= （マイナスになったら？ゼロになったら？）
+    #     履歴保存.id
+
   def destroy
+    user_items = UserItem.find(params[:id])
+    user_items.destroy
+    redirect_to lovers_end_path
   end
 
   def update
