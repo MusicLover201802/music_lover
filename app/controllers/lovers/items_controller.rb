d2683347bf7904df81810be9f2687ca30caeb9d0
takuma1012 <stealth.taku@gmail.com>
2018-03-17 13:00:19 +0000

    今日はここまで

--- app/controllers/lovers/items_controller.rb
+++ app/controllers/lovers/items_controller.rb
@@ -4,5 +4,9 @@ class Lovers::ItemsController < ApplicationController
 
   def show
   	@item = Item.find(params[:id])
+  	genres = Item.find_by_genre_id(params[:id])
+  	@genre = Genre.find_by_id(genres)
+  	disc = Disc.where(item_id: "@item")
+  	@track = Track.where(disc_id: disc)
   end
 end
