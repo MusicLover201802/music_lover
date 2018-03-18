cf6e425479b0f4f41b2c4d6a2112f469969476e1
takuma1012 <stealth.taku@gmail.com>
2018-03-18 14:44:10 +0000

    Finished index genre

--- app/controllers/lovers/genres_controller.rb
+++ app/controllers/lovers/genres_controller.rb
@@ -1,5 +1,7 @@
 class Lovers::GenresController < ApplicationController
   def show
-  	@items = Item.where("item_name like ?", "artist_name like ?").search(params[:search])
+  	# @items = Item.where("item_name like ?", "artist_name like ?").search(params[:search])
+  	@genre = Genre.find(params[:id])
+  	@items = Item.where(id: @genre)
   end
 end
--- app/controllers/lovers/items_controller.rb
+++ app/controllers/lovers/items_controller.rb
@@ -1,12 +1,11 @@
 class Lovers::ItemsController < ApplicationController
   def index
+  	@items = Item.all
   end
 
   def show
-  	@item = Item.find(params[:id])
+  	@item  = Item.find(params[:id])
   	genres = Item.find_by_genre_id(params[:id])
   	@genre = Genre.find_by_id(genres)
-  	# disc = Disc.where(item_id: "@item")
-  	# @track = Track.where(disc_id: disc)
   end
 end
