a1f7d5ac8fbee031407cc17c3eafc3643113676d
Kaito-Fukai <yaiag04@gmail.com>
2018-03-22 06:29:57 +0000

    購入確定時の4アクション完成！これからカート内数量増減機能作成へ

--- app/controllers/lovers/orders_controller.rb
+++ app/controllers/lovers/orders_controller.rb
@@ -1,64 +1,100 @@
 class Lovers::OrdersController < ApplicationController
+# アクション名は仮です
 
+	# 購入確認ページ表示:リダイレクト元を取得し、どこから遷移してきたかによって
+	# ビューページに渡す送り先情報を切り替えている
 	def new
 		@neworder = Order.new
 		path = Rails.application.routes.recognize_path(request.referer)
 
-			if session[:create] == "create"#path[:controller] == 'lovers/destinations' && path[:action] == 'create'
-				session[:create] = nil
-				@neworder.last_name = '新規送り先を登録時　仮表示'
-				@neworder.first_name = 'test'
-				@neworder.last_name_kana = 'test'
-				@neworder.first_name_kana = 'test'
-				@neworder.postal_code = 'test'
-				@neworder.prefecture = 'test'
-				@neworder.city = 'test'
-				@neworder.building = 'test'
-				@neworder.phone_number = 'test'
+		if session[:create] == "create"
+			session[:create] = nil
+			new_destination = Destination.order("created_at DESC").find_by(user_id: current_user.id)
+			@neworder.last_name = new_destination.last_name
+			@neworder.first_name =  new_destination.first_name
+			@neworder.last_name_kana =  new_destination.last_name_kana
+			@neworder.first_name_kana =  new_destination.first_name_kana
+			@neworder.postal_code =  new_destination.postal_code
+			@neworder.prefecture =  new_destination.prefecture
+			@neworder.city =  new_destination.city
+			@neworder.building =  new_destination.building
+			@neworder.phone_number =  new_destination.phone_number
 
-				elsif path[:controller] == 'lovers/destinations' && path[:action] == 'new'
-					d = Destination.find_by(id: params[:id])
-					@neworder.last_name = d.last_name
-					@neworder.first_name = d.first_name
-					@neworder.last_name_kana = d.last_name_kana
-					@neworder.first_name_kana = d.first_name_kana
-					@neworder.postal_code = d.postal_code
-					@neworder.prefecture = d.prefecture
-					@neworder.city = d.city
-					@neworder.building = d.building
-					@neworder.phone_number = d.phone_number
+		elsif path[:controller] == 'lovers/destinations' && path[:action] == 'new'
+			d = Destination.find_by(id: params[:id])
+			@neworder.last_name = d.last_name
+			@neworder.first_name = d.first_name
+			@neworder.last_name_kana = d.last_name_kana
+			@neworder.first_name_kana = d.first_name_kana
+			@neworder.postal_code = d.postal_code
+			@neworder.prefecture = d.prefecture
+			@neworder.city = d.city
+			@neworder.building = d.building
+			@neworder.phone_number = d.phone_number
 
+		else
+			@user = User.find(current_user.id)
+			@neworder.last_name = @user.last_name
+			@neworder.first_name = @user.first_name
+			@neworder.last_name_kana = @user.last_name_kana
+			@neworder.first_name_kana = @user.first_name_kana
+			@neworder.postal_code = @user.postal_code
+			@neworder.prefecture = @user.prefecture
+			@neworder.city = @user.city
+			@neworder.building = @user.building
+			@neworder.phone_number = @user.phone_number
 
-					else
-						@user = User.find(current_user.id)
-						@neworder.last_name = @user.last_name
-						@neworder.first_name = @user.first_name
-						@neworder.last_name_kana = @user.last_name_kana
-						@neworder.first_name_kana = @user.first_name_kana
-						@neworder.postal_code = @user.postal_code
-						@neworder.prefecture = @user.prefecture
-						@neworder.city = @user.city
-						@neworder.building = @user.building
-						@neworder.phone_number = @user.phone_number
-			end
+		end
+
 		@user = User.find(current_user.id)
 		@user_items = @user.user_items
 	end
-
 
 
 	def show
 		@order = Order.find(params[:id])
 	end
 
+
+	# 購入履歴(Orders)を保存する処理
 	def create
-		@neworder = Order.new
-		@neworder.save
-		@neworderitem = OrderItem.new(order_item_params)
-		@neworderitem.save
-		redirect_to controller: 'user_items', action: 'destroy', id: current_user.id
+	    @neworder = Order.new(order_params)
+	    @neworder.user_id = current_user.id
+	    @neworder.save
+	    redirect_to lovers_orders_orderitems_save_path
 	end
 
+	# 購入明細(Order_items)を保存する処理
+	# order_idは、カレントユーザーの直近の注文idを取ってくるよう設定しました
+	# 流れ：①過去の注文履歴（Orders)を新しい順に並べ→②カレントユーザーの注文1件を抜き出し→③そのorder_idをOrderItemに渡しています
+	def orderitems_save
+	 	@user_items = UserItem.where(user_id: current_user.id)
+	    past_order = Order.order("created_at DESC").find_by(user_id: current_user.id)
+	    @user_items.each do |user_items| # カートの中身1レコードごとにorder_itemsテーブルのインスタンスをnew → save
+	    	@neworderitem = OrderItem.new(item_id: user_items.item_id,
+	    								  quantity: user_items.quantity,
+	    								  price: user_items.item.price,
+	    								  order_id: past_order.id)
+	    	@neworderitem.save
+	    end
+	  	redirect_to lovers_user_items_cart_stock_path
+	end
+
+
+  private
+	def order_params
+		params.require(:order).permit(:last_name,
+									  :first_name,
+									  :last_name_kana,
+									  :first_name_kana,
+									  :postal_code,
+									  :prefecture,
+									  :city,
+									  :building,
+									  :phone_number,
+									  :payment,
+									  :user_id)
+	end
 
 end
 
--- app/controllers/lovers/user_items_controller.rb
+++ app/controllers/lovers/user_items_controller.rb
@@ -2,70 +2,72 @@
 class Lovers::UserItemsController < ApplicationController
 before_action :get_current_cart
 
+  def check #進捗確認用　開発終了したら削除します
+    @order_items = OrderItem.all
+    @orders = Order.all
+    @items = Item.all
+  end
+
   # カートの中身全取得 → @user_items変数に格納
   def get_current_cart
     @user = User.find(current_user.id)
-    @user_items = @user.user_items
+    @user_items = UserItem.where(user_id: current_user.id)
   end
 
-
-  #注文数に応じて在庫を減らすアクション
+  #購入確定→購入アクション①注文数に応じて在庫を減らすアクション
   def cart_stock
     @user_items.each do |user_items| #1レコードごとに、itemsテーブルのstockカラム - カートの中の数量 を実行し、save
     user_items.item.stock -= user_items.quantity
     user_items.item.save
     end
-    redirect_to lovers_end_path
+    redirect_to lovers_user_items_cart_destroy_path
   end
 
-
-  # 購入履歴を保存する処理　送り先をデフォルトにしない場合の設定はこれからです
-  def create
-      @neworder = Order.new(order_params)
-      @neworder.user_id = current_user.id
-
-      binding.pry
-      
-      @neworder.save
-  end
-
-  # 購入明細を保存する処理
-  # order_idは、カレントユーザーの直近の注文idを取ってくるよう設定しました
-  # 流れ：①過去の注文履歴（Orders)を新しい順に並べ→②カレントユーザーの注文1件を抜き出し→③そのorder_idをOrderItemに渡しています
-  def orderitems_save
-    past_order = Order.order("created_at DESC").find_by(user_id: current_user.id)
-    @user_items.each do |user_items| # カートの中身1レコードごとにorder_itemsテーブルのインスタンスをnew → save
-      @neworderitem = OrderItem.new(item_id: user_items.item_id, quantity: user_items.quantity, price: user_items.item.price, order_id: past_order.id)
-      @neworderitem.save
-    end
-  redirect_to lovers_end_path
-  end
-
-  #カートの中身を空にするアクション
+  #購入アクション②カートの中身を空にするアクション
   def cart_destroy
+    @user = User.find(current_user.id)
+    @user_items = @user.user_items
     @user_items.each do |user_items|  #カート中身を1レコードずつ削除していく
+      binding.pry
       user_items.destroy
     end
     redirect_to lovers_end_path
   end
 
-  def check #進捗確認用　開発終了したら削除します
-    @order_items = OrderItem.all
-    @orders = Order.all
+  # カートの中身を更新するアクション:id1のものしか更新できないのが課題
+  def update
+    item = UserItem.find(params[:id])
+    item.update(quantity_params)
+
+    item.save
+    redirect_to lovers_user_item_path(current_user.id)
   end
+
+# カートの中身を新規に生成するアクション 作成中
+  def add_item
+    @add_item = UserItem.new(add_item_params)
+    @add_item.user_id = current_user.id
+
+    @add_item.save
+    redirect_to  lovers_user_item_path(current_user.id)
+  end
+
 
   def show
   end
 
+private
 
-  def update
+  def user_item_params
+    params.require(:user_item).permit(:id)
   end
 
-
-  private
-  def order_params
-    params.require(:order).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :prefecture, :city, :building, :phone_number, :payment, :user_id)
+  def quantity_params
+    params.require(:user_item).permit(:quantity)
   end
 
+  def add_item_params
+    params.requre(:user_item).permit(:item_id)
+  end
 
 end
