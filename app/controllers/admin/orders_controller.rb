d590e9e1d6e1eeeea09d8a7365751a6268f334e6
akatuki06 <akatuki0623@gmail.com>
2018-03-23 17:27:26 +0000

    test

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,12 @@
 class Admin::OrdersController < ApplicationController
   def index
-    @orders = Order.all
+    @orders = Order.page(params[:page]).per(5)
+  end
+
+   def create
+    @order = Order.new(order_params)
+    @order.save
+    redirect_to admin_orders_path
   end
 
   def show
@@ -8,12 +14,33 @@ class Admin::OrdersController < ApplicationController
   end
 
   def edit
-  end
-
-  def create
+    @order = Order.find(params[:id])
   end
 
   def update
+      order = Order.find(params[:id])
+      order.update(order_params)
+      redirect_to admin_orders_path
   end
-  
+
+  private
+
+  def order_params
+    params.require(:order).permit(:last_name_kana, 
+                                  :first_name_kana, 
+                                  :created_at, 
+                                  :status, 
+                                  :payment, 
+                                  :postal_code, 
+                                  :prefecture, 
+                                  :city, 
+                                  :building, 
+                                  :phone_number, 
+                                  order_items_attributes: [:id, 
+                                               :item_id, 
+                                               :quantity, 
+                                               :price, 
+                                               :order_id]
+      )
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ApplicationRecord
 	belongs_to :user
 	has_many :order_items
 	has_many :items, through: :order_items
+
+	accepts_nested_attributes_for :order_items, allow_destroy: true
 end
