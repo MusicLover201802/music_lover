0792d0388d4d7b1e7681a3d8ab91988ad1489c2c
saya08 <s88hata@gmail.com>
2018-03-23 09:22:55 +0000

    購入履歴詳細

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,12 @@
 class Admin::OrdersController < ApplicationController
   def index
-    @orders = Order.all
+    @orders = Order.page(params[:page]).per(5)
+  end
+
+   def create
+    @order = Order.new(post_params)
+    @order.save
+    redirect_to admin_orders_path
   end
 
   def show
@@ -8,12 +14,18 @@ class Admin::OrdersController < ApplicationController
   end
 
   def edit
-  end
-
-  def create
+    @order = Order.find(params[:id])
   end
 
   def update
+      order = Order.find(params[:id])
+      order.update(post_params)
+      redirect_to admin_orders_path
   end
-  
+
+  private
+
+  def post_params
+    params.require(:order).permit(:last_name_kana, :first_name_kana, :created_at, :status, :payment, :postal_code, :prefecture, :city, :building, :phone_number)
+  end
 end
