87f45685cd12d1ba57b9a799b908d7f5f9a924a7
saya08 <s88hata@gmail.com>
2018-03-24 12:57:00 +0000

    帰ります

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -19,13 +19,6 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-     if @item.save
-    redirect_to admin_items_path
-    else
-    @items = Item.all.reverse_order
-    render :new
-    end
-
   end
 
   def update
