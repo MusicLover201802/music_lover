3067e415afc72fe390c8a10912f8f5549974428f
akatuki06 <akatuki0623@gmail.com>
2018-03-25 11:16:01 +0000

    カート追加重複問題解消

--- app/controllers/lovers/user_items_controller.rb
+++ app/controllers/lovers/user_items_controller.rb
@@ -1,6 +1,6 @@
 # 開発中。アクション名は仮です。最終的にコントローラーを分ける可能性もあります
 class Lovers::UserItemsController < ApplicationController
-before_action :get_current_cart
+before_action :get_current_cart!
 
   def check #進捗確認用　開発終了したら削除します
     @order_items = OrderItem.all
@@ -8,11 +8,7 @@ before_action :get_current_cart
     @items = Item.all
   end
 
-  # カートの中身全取得 → @user_items変数に格納
-  def get_current_cart
-    @user = User.find(current_user.id)
-    @user_items = UserItem.where(user_id: current_user.id)
-  end
+  
 
   def show
   end
@@ -29,17 +25,33 @@ before_action :get_current_cart
 
   # カート画面から、カート内数量を→更新するアクション
   def update
-    @new_order = UserItem.find_by(user_item_params)
-    @new_order.update(quantity_params)
-    @new_order.save
+    @user_items = UserItem.find_by(user_item_params)
+    @user_items.update(quantity_params)
+    @user_items.save
     redirect_to lovers_user_item_path(current_user.id)
   end
 
   # 商品詳細から、商品をカートに入れる
+  # さの修正
   def create
-    @new_order = UserItem.new(add_item_params)
-    @new_order.user_id = current_user.id
-    @new_order.save
+
+    # binding.pry
+    item_id = params[:user_item][:item_id].to_i
+    if @user_items.find_by(item_id: item_id).blank?
+
+    @user_items = UserItem.new(add_item_params)
+    @user_items.user_id = current_user.id
+    @user_items.save
+
+    else
+      # binding.pry
+    @user_item = @user_items.find_by(item_id: item_id)
+    @user_item.quantity += params[:user_item][:quantity].to_i
+    @user_item.user_id = current_user.id
+    @user_item.save
+  end
+
+
     redirect_to lovers_user_item_path(current_user.id)
   end
 
@@ -62,7 +74,13 @@ private
   end
 
   def add_item_params
-    params.require(:user_item).permit(:quantity, :item_id)
+    params.require(:user_item).permit(:quantity, :item_id,)
+  end
+
+  # カートの中身全取得 → @user_items変数に格納
+  def get_current_cart!
+    @user = User.find(current_user.id)
+    @user_items = UserItem.where(user_id: current_user.id)
   end
 
 end
