cb6b5f8f30ff7826317b5fbdd709c5e8a54ce4ff
akatuki06 <akatuki0623@gmail.com>
2018-03-26 06:18:32 +0000

    test3

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,9 +1,13 @@
 class Admin::ItemsController < ApplicationController
-  def index
-            ### 検索用 ###
-    @search = Itemgit .ransack(params[:q])
-    @selects = @search.result.page(params[:page]).per(5).reverse_order
 
+    before_action :authenticate_admin!
+
+  def index
+    # @items = Item.all.reverse_order
+    #@items = Item.page(params[:page]).per(5).reverse_order
+            ### 検索用 ###
+    @search = Item.ransack(params[:q])
+    @selects = @search.result.page(params[:page]).per(5).reverse_order
 
   end
 
@@ -44,13 +48,6 @@ class Admin::ItemsController < ApplicationController
    redirect_to admin_items_path
   end
 
-
-  def retire
-    item = Item.find(params[:id])
-    item.update(retire_flag: true) ### リタイアフラグをtrueに更新
-    item.soft_destroy ### 論理削除処理※soft_destroyはkakurenbo-putiの固有メソッド
-    redirect_to lovers_retire_path ### 言わずもがな遷移先
-  end
 
   private
 
@@ -97,5 +94,4 @@ end
 #               }
 #               }
 #               } permitted: false>
-
 
