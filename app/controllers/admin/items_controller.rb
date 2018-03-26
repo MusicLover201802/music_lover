1aad7fa87ae790d15b29087c943b9934ae99abe9
akatuki06 <akatuki0623@gmail.com>
2018-03-26 05:17:00 +0000

    商品編集修正2

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,12 +3,9 @@ class Admin::ItemsController < ApplicationController
     before_action :authenticate_admin!
 
   def index
-    # @items = Item.all.reverse_order
-    #@items = Item.page(params[:page]).per(5).reverse_order
             ### 検索用 ###
     @search = Item.ransack(params[:q])
     @selects = @search.result.page(params[:page]).per(5).reverse_order
-
   end
 
   def show
@@ -39,7 +36,7 @@ class Admin::ItemsController < ApplicationController
   def update
     item = Item.find(params[:id])
     item.update(item_params)
-    redirect_to admin_items_path
+    redirect_to admin_item_path(params[:id])
   end
 
   def destroy
@@ -47,6 +44,14 @@ class Admin::ItemsController < ApplicationController
    item.destroy
    redirect_to admin_items_path
   end
+
+  def retire
+    item = Item.find(params[:id])
+    item.update(retire_flag: true) ### リタイアフラグをtrueに更新
+    item.soft_destroy ### 論理削除処理※soft_destroyはkakurenbo-putiの固有メソッド
+    redirect_to lovers_retire_path ### 言わずもがな遷移先
+  end
+
 
 
   private
